5dde3b7cb5df22cdc7c4241072533e045ffb982b
Nilay Singh <snilay011@gmail.com>
2024-04-02 11:20:38 +0530

    vid push

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import PreviewPlayer from "./components/PreviewPlayer";
+
+function App() {
+  return (
+    <div className="app h-[100vh] w-full">
+       <PreviewPlayer/>
+    </div>
+  );
+}
+
+export default App;
